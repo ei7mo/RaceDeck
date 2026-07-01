69b520ee783361ba10bc60d8cfeea805453a6ee3
Eric-Miles18 <ericmiles199@gmail.com>
2026-07-01 20:03:48 +0300

    Remover the navbar from the page itself

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,10 @@
 import RaceCard from "../components/RaceCard";
-import Navbar from "../components/Navbar";
 
 function Home() {
   return (
-    <>
-      <Navbar />
-      <main className="text-txt-primary grid grid-cols-1 md:grid-cols-2 md:grid-row-11 xl:grid-cols-4 xl:grid-rows-6 gap-10 my-7 mx-6">
-        <RaceCard />
-      </main>
-    </>
+    <main className="text-txt-primary grid grid-cols-1 md:grid-cols-2 md:grid-row-11 xl:grid-cols-4 xl:grid-rows-6 gap-10 my-7 mx-6">
+      <RaceCard />
+    </main>
   );
 }
 
